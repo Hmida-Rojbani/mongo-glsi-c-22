1286e4dcebf844086685690b1ab9a4a30503f482
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-02-15 13:26:47 +0100

    phase 1 connection

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+//for localhost connection
+//mongodb://localhost:27017/bdname
 mongoose.connect('mongodb+srv://user:1234@db.mhbax.mongodb.net/gl-c-22?retryWrites=true&w=majority')
     .then(()=>console.log('MongoDB is up.'))
     .catch((err)=>console.log('MongoDB is Down, raison :',err.message));
