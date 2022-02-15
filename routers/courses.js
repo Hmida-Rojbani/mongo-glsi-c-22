25c90d836fc035f2238ad90e280e9341b8e13d7f
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-02-15 15:39:33 +0100

    add get

--- routers/courses.js
+++ routers/courses.js
@@ -2,9 +2,19 @@ const router = require('express').Router();
 const {Course} = require('../models/course');
 
 router.post('',async (req,res)=>{
-    let course = new Course(req.body);
-    course =  await course.save();
-    res.send(course);
+    try {
+        let course = new Course(req.body);
+        course =  await course.save();
+        res.send(course);
+    } catch (error) {
+        res.status(400).send('Proble saving Course : '+error.message)
+    }
+    
+});
+
+router.get('',async (req,res)=>{
+    let courses = await Course.find({},'title author isPublished -_id');
+    res.send(courses);
 });
 
 module.exports=router;
