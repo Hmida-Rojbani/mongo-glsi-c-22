de4f8dba79c287ff7f98614beed084b7a0520500
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-02-15 16:31:39 +0100

    add joi

--- models/course.js
+++ models/course.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 
 const course_schema = new mongoose.Schema({
     title : String,
@@ -12,6 +13,15 @@ const course_schema = new mongoose.Schema({
     price : Number
 });
 
+const course_validation = Joi.object({
+    title : Joi.string().min(3).required(),
+    author : Joi.string().min(5).max(30).required(),
+    tags : Joi.array().items(Joi.string().alphanum()).required(),
+    isPublished : Joi.boolean(),
+    price : Joi.number().positive()
+});
+
 const Course = mongoose.model('Course', course_schema);
 
-module.exports.Course = Course;
+module.exports.Course = Course; 
+module.exports.course_validation = course_validation; 
--- routers/courses.js
+++ routers/courses.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const {Course} = require('../models/course');
+const {Course, course_validation} = require('../models/course');
 
+// add course to DB
+// add Joi 
 router.post('',async (req,res)=>{
     try {
         let course = new Course(req.body);
@@ -12,8 +14,34 @@ router.post('',async (req,res)=>{
     
 });
 
+// get all courses
 router.get('',async (req,res)=>{
     let courses = await Course.find({},'title author isPublished -_id');
+    res.send(courses);
+});
+// get title starts with a prefix
+router.get('/title/starts/:prefix',async (req,res)=>{
+    let courses = await Course.find({title : new RegExp('^'+req.params.prefix,'i')});
+    res.send(courses);
+});
+
+// get with price over
+router.get('/price/over/:p',async (req,res)=>{
+    let courses = await Course.find({price : { $gte : req.params.p}});
+    res.send(courses);
+});
+
+// get with price under
+// operations 
+// ==  | $eq
+// !=  | $neq
+// > | $gt
+// >= | $gte
+// < | $lt
+// <= | $lte
+// $in $nin
+router.get('/price/under/:p',async (req,res)=>{
+    let courses = await Course.find({price : { $lte : req.params.p}});                                
     res.send(courses);
 });
 
