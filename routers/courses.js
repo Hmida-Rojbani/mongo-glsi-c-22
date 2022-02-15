289e94f1d486a9f3036ea409273f01c2dd756425
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-02-15 14:42:15 +0100

    post

--- routers/courses.js
+++ routers/courses.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+const {Course} = require('../models/course');
 
-
+router.post('',async (req,res)=>{
+    let course = new Course(req.body);
+    course =  await course.save();
+    res.send(course);
+});
 
 module.exports=router;
