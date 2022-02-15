2b113904502452d75c8fbe7db938616fec1e364f
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-02-15 14:24:21 +0100

    add router

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('./db/connect');
 const express = require('express');
+const course_router= require('./routers/courses')
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
-
+app.use('/api/courses',course_router);
 
 app.listen(port, () =>  console.log(`Server running on ${port}`));
