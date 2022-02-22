7bfd7dfddad004fc7a4b9681dde721882ca7b36b
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-02-22 13:40:12 +0100

    add validation

--- routers/courses.js
+++ routers/courses.js
@@ -5,11 +5,14 @@ const {Course, course_validation} = require('../models/course');
 // add Joi 
 router.post('',async (req,res)=>{
     try {
+        let result_valid= course_validation.validate(req.body);
+        if(result_valid.error)
+            return res.status(400).send(result_valid.error.details[0].message);
         let course = new Course(req.body);
         course =  await course.save();
-        res.send(course);
+        res.status(201).send(course);
     } catch (error) {
-        res.status(400).send('Proble saving Course : '+error.message)
+        res.status(400).send('Problem saving Course : '+error.message)
     }
     
 });
@@ -44,5 +47,7 @@ router.get('/price/under/:p',async (req,res)=>{
     let courses = await Course.find({price : { $lte : req.params.p}});                                
     res.send(courses);
 });
+
+
 
 module.exports=router;
